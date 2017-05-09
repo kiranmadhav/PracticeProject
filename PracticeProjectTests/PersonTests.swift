0981652c01d27934f96f3ae836681c30e3219ce2
Kiran  Madhav <kiranmadhav@kirans-macbook-pro.local>
2017-05-09 21:44:08 +0530

    changed the name

--- PracticeProjectTests/PersonTests.swift
+++ PracticeProjectTests/PersonTests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-@testable import PracticeProjec
+@testable import PracticeProject
 
 class PersonTests: XCTestCase {
     
--- PracticeProjectTests/ViewControllerTests.swift
+++ PracticeProjectTests/ViewControllerTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import PracticeProjec
+@testable import PracticeProject
 
 class ViewControllerTests: XCTestCase {
     
